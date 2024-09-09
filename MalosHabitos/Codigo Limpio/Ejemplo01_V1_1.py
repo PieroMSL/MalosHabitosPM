bd7ef3e191a3c8c7ae3058e0eba4d92b12a8d4db
sebas <olazo.lope@gmail.com>
2024-09-09 18:17:23 -0500

    Refactorizacion Ejemplo 01

--- MalosHabitos/Codigo Limpio/Ejemplo01_V1_1.py
+++ MalosHabitos/Codigo Limpio/Ejemplo01_V1_1.py
@@ -1,11 +1,11 @@
-def multiplicación(factor1, factor2):
-    producto = factor1 * factor2
+def multiplicación(multiplicando, multiplicador):
+    producto = multiplicando * multiplicador
     return producto
 
 
 if __name__ == "__main__":
-    multiplicando = float(input("Multiplicando:"))
-    multiplicador = float(input("Multiplicador:"))
+    multiplicando = float(input("Multiplicando: "))
+    multiplicador = float(input("Multiplicador: "))
     resultado = multiplicación(multiplicando, multiplicador)
 
     print(f"{multiplicando} * {multiplicador} = {resultado}")
